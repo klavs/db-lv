a8564cf995752e74612b1e172c3af35ad2726c97
Klāvs Priedītis <klavs.pr@gmail.com>
2017-08-13 20:05:56 +0300

    Generate short names

---
+++ data/raw/short-names.js
@@ -0,0 +1,13 @@
+const fs = require("fs")
+const groups = require("../danceGroups.json")
+const rx = /"([^\"]+)"[^\"]*$/
+groups.forEach(g => {
+    const res = rx.exec(g.fullName)
+    if (!res || res.length == 0){
+        console.log(g.fullName)
+    } else {
+        g.shortName = res[1]
+    }
+})
+
+fs.writeFileSync("../danceGroups.json", JSON.stringify(groups, null, 4))
